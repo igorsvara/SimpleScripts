b4e8337f3da2d3c01d85638fb02d41325bbf1b67
igor <svaraigorw@gmail.com>
2024-03-01 16:11:30 +0100

    added upper.py

--- upper.py
+++ upper.py
@@ -1,15 +1,14 @@
 def function():
+    f_name = input("The full name of the file: ")
 
-	f_name = input("The full name of the file: ")
+    f = open(f_name, "r")
+    f.seek(0)
+    content = f.read()
+    f.close()
 
-	f = open(f_name, "r")
-	f.seek(0)
-	content = f.read() 
-	f.close()
-
-	out = open(f_name, "w")
-	out.write(content.upper())
-	out.close()	
+    out = open(f_name, "w")
+    out.write(content.upper())
+    out.close()
 
 
 function()
