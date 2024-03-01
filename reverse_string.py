0ac34bba713fcb48ed6fd222a72e984148202a7f
igor <svaraigorw@gmail.com>
2024-03-01 16:10:43 +0100

    added reverse_string.py

---
+++ reverse_string.py
@@ -0,0 +1,8 @@
+def reverse_string():
+    input_string = input("Enter a string: ")
+    reversed_string = input_string[::-1]
+
+    print(f"The reversed string is: {reversed_string}")
+
+
+reverse_string()
